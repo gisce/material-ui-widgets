331fbd471b3e84582f5b0a10f7ad70103605280c
Xavi Torelló <info@xaviertorello.cat>
2016-05-06 11:42:41 +0200

    Widget NotificationBadge created
    Create a Notification Badge that uses Badges, Menu, MenuItem and Icons to provide a fully-functional widget.
    
    Clicking on the badge show a floating div containing all the notifications.
    
    ```
    import NotificationBadge from 'material-ui/Widgets/NotificationBadge';
    
    import IconNotification from 'material-ui/svg-icons/social/notifications';
    
    var notifications = [
    {
        subject: 'Nova proposta 15-05-2015 creada',
        className: 'fa fa-users text-aqua'
    }, {
        subject: 'Càrrega d\'F1s finalitzada',
        className: 'fa fa-warning text-yellow'
    }, {
        subject: 'Nova proposta 14-05-2015 creada',
        className: 'fa fa-users text-red'
    },];
    
    <NotificationBadge notificationsList={notifications} icon={<IconNotification />} tooltip="Notifications" textColor="white" badgeColor={deepOrange500}/>
    ```

---
+++ Widgets/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _Notification = require('./NotificationBadge');
+
+var _Notification2 = _interopRequireDefault(_Notification);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Notification2.default;
