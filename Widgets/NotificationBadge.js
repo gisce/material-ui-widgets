c89873d85ff6b9d44b3995fc93651f433684d824
Xavi Torelló <info@xaviertorello.cat>
2016-07-19 14:00:09 +0200

    NotificationBadge enhaced style with avatar and time
    Now is rendered using a listItem with
    * leftAvatar (if exists)
    * primaryText
    * secondatyText

--- Widgets/NotificationBadge.js
+++ Widgets/NotificationBadge.js
@@ -63,8 +63,11 @@ var _badge2 = _interopRequireDefault(_badge);
 var _iconButton = require('../IconButton');
 var _iconButton2 = _interopRequireDefault(_iconButton);
 
-var _fontIcon = require('../FontIcon')
-var _fontIcon2 = _interopRequireDefault(_fontIcon);
+var _listItem = require('../List/ListItem')
+var _listItem2 = _interopRequireDefault(_listItem);
+
+var _avatar = require('../Avatar')
+var _avatar2 = _interopRequireDefault(_avatar);
 
 function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {default: obj};
@@ -224,19 +227,30 @@ var NotificationBadge = function (_React$Component) {
                     )
                 );
 
-                //The elements to notify using the subject key
                 this.state.notifications.forEach(function (notification, index) {
 
+                    //The avatar if exists (notification.displayPicture)
+                    var avatar = (notification.displayPicture != undefined)?
+                        _react2.default.createElement(
+                            _avatar2.default,
+                            {
+                                src:notification.displayPicture
+                            }
+                        ):null;
+
+                    //The elements to notify using the subject key
+                    // If there are a messageTime defined, include it as a secondary text
                     var entry = _react2.default.createElement(
-                        _menuItem2.default,
+                        _listItem2.default,
                         {
                              key: _props.tooltip + index,
-                        },
-                        (notification.from != undefined)?'@' + notification.from + ' | ':'',
-                        notification.subject,
-                        (notification.messageTime != undefined)? ' | ' + notification.messageTime:''
+                             leftAvatar: avatar,
+                             secondaryText: notification.messageTime,
+                             primaryText: notification.subject
+                        }
 
                     );
+
                     menu_entries.push(entry);
                 });
 
